693050b23a9aafc4ac669b2a54fd500074ce50a5
Arash7447 <arash.sotoudeh6@gmail.com>
2024-06-12 16:02:02 +0330

    added html codes
    added html codes

--- employees/urls.py
+++ employees/urls.py
@@ -1,12 +1,18 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, DepartmentViewSet, DeptManagerViewSet, DeptEmpViewSet, TitleViewSet, SalaryViewSet
+from .views import EmployeeViewSet, DepartmentViewSet, DeptManagerViewSet, DeptEmpViewSet, TitleViewSet, SalaryViewSet, employee_list
+from . import views
 
 router = DefaultRouter()
 
 router.register(r'employees', EmployeeViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
+    path('employee-list/', employee_list, name='employee-list'),
+    path('employee-detail/<int:pk>/', views.employee_detail_html, name='employee-detail-html'),
+    path('employee-update/<int:pk>/', views.employee_update_html, name='employee-update-html'),
+    path('employee-delete/<int:pk>/', views.employee_delete_html, name='employee-delete-html'),
+    
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,12 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Employee, Department, DeptEmp, DeptManager, Salary, Title
 from .serializers import EmployeeSerializer, DepartmentSerializer, DeptEmpSerializer, DeptManagerSerializer, SalarySerializer, TitleSerializer
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from django_elasticsearch_dsl.registries import registry
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
@@ -38,8 +54,39 @@ def search(request):
     return render(request, 'search_results.html', {'employees': employees})
 
 def employee_list(request):
-    employee_list = Employee.objects.all()
-    paginator = Paginator(employee_list, 10)  
+    employee_objects = Employee.objects.all()
+    paginator = Paginator(employee_objects, 10)  
     page_number = request.GET.get('page')
-    employees = paginator.get_page(page_number)
+    
+    try:
+        employees = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        employees = paginator.page(1)
+    except EmptyPage:
+        employees = paginator.page(paginator.num_pages)
+    
     return render(request, 'employees/employee_list.html', {'employees': employees})
+
+def employee_detail_html(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    return render(request, 'employees/employee_detail.html', {'employee': employee})
+
+
+def employee_update_html(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee-detail-html', pk=pk)
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'employees/employee_update.html', {'form': form})
+
+
+def employee_delete_html(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee-list')  
+    return render(request, 'employees/employee_delete.html', {'employee': employee})
